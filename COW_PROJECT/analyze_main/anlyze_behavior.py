2004fc18389e47b27f54f681d66c3188db2df495
Monta <>
2019-05-15 14:34:10 +0900

    5/14 道のりの要素を3次元プロットに追加

--- COW_PROJECT/analyze_main/anlyze_behavior.py
+++ COW_PROJECT/analyze_main/anlyze_behavior.py
@@ -16,7 +16,7 @@ def show_3d_plot(df):
 
     a = translate_dt_to_int(df['A'])
     # 散布図作成
-    ax.plot(a, df['I'], df['K'], "o", color="#00aa00", ms=4, mew=0.5)
+    ax.plot(a, df['J'], df['L'], "o", color="#00aa00", ms=4, mew=0.5)
     #ax.plot(df['D'], df['H'], df['K'], "o", color="#00aa00", ms=4, mew=0.5)
 
     plt.show()
@@ -29,5 +29,5 @@ def translate_dt_to_int(dt_list):
     return pd.Series(i_list)
 
 if __name__ == '__main__':
-    df = pd.read_csv(filepath_or_buffer = "feature.csv", encoding = "utf-8", sep = ",", header = 0, usecols = [0,3,7,8,10], names=('A', 'D', 'H', 'I', 'K'))
+    df = pd.read_csv(filepath_or_buffer = "feature.csv", encoding = "utf-8", sep = ",", header = 0, usecols = [0,3,7,8,9,11], names=('A', 'D', 'H', 'I', 'J','L'))
     show_3d_plot(df)
