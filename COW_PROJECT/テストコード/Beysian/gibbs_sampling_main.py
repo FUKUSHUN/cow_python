5dd8a4b0e31ef296df4328674ace12e2c7dffde6
MONTA-PC\Monta <sfukumoto123@gmail.com>
2020-01-15 15:37:37 +0900

    1/15 ポアソン混合モデルのギブスサンプリングアルゴリズムと新規データの予測処理を実装

--- COW_PROJECT/テストコード/Beysian/gibbs_sampling_main.py
+++ COW_PROJECT/テストコード/Beysian/gibbs_sampling_main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 # 自作クラス
 import myClass.plotting as plotting
+import myClass.mixed_model as mixed_model
 
 def create_artificial_data(lam, num):
     """ テスト用のデータセットを作成する
@@ -13,14 +14,51 @@ def create_artificial_data(lam, num):
     X = np.random.poisson(lam, num) # ndarray
     return X
 
+def extract_data(X, S, k):
+    N = len(X.T)
+    new_X = []
+    for n in range(N):
+        if (S[k, n] == 1):
+            new_X.append(X[0,n])
+    return new_X
+
 if __name__ == '__main__':
     # 多峰性の1次元データ点を生成
     X1 = create_artificial_data(20, 1000)
     X2 = create_artificial_data(50, 750)
     X = np.hstack((X1, X2)) # 2つのndarrayを結合
     np.random.shuffle(X) # データをシャッフル
-    
+    X = np.array([X]) # データの2次元化
+
     # データを可視化
     plotter = plotting.PlotUtility()
     plotter.hist_plot([X1,X2], 20, color=None) # ヒストグラムを表示，正解で色分け
+
+    # ポアソン混合モデルのパラメータの設定
+    lambda_vector = np.array([30, 40])
+    pi_vector = np.array([0.5, 0.5])
+    alpha_vector = np.array([1, 1])
+    max_iterater = 50
+
+    # ギブスサンプリングによるクラスタリング
+    a_0, b_0 = 1, 1
+    poisson_model = mixed_model.PoissonMixedModel(lambda_vector, pi_vector, alpha_vector, max_iterater)
+    result = poisson_model.gibbs_sample(X, a_0, b_0)
+    new_X = np.array([np.arange(1,100)])
+    prob_matrix = poisson_model.predict(new_X)
+
+    # クラスタリング結果を可視化
+    X1 = extract_data(X, result, 0)
+    X2 = extract_data(X, result, 1)
+    plotter2 = plotting.PlotUtility()
+    plotter2.hist_plot([X1,X2], 20, color=None)
+
+    plotter_prob = plotting.PlotUtility()
+    prob1, prob2 = prob_matrix[0,:], prob_matrix[1,:]
+    plotter_prob.scatter_plot(new_X, prob1, [0 for _ in range(len(new_X))])
+    plotter_prob.scatter_plot(new_X, prob2, [1 for _ in range(len(new_X))])
+
+    # 表示
     plotter.show()
+    plotter2.show()
+    plotter_prob.show()
--- COW_PROJECT/テストコード/Beysian/myClass/mixed_model.py
+++ COW_PROJECT/テストコード/Beysian/myClass/mixed_model.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 from scipy import stats # カテゴリ分布から乱数を生成
 
@@ -8,6 +9,8 @@ class PoissonMixedModel:
     pi_vector = None # K次元ベクトル  カテゴリ分布の各生起確率のベクトル
     _alpha_vector = None # K次元ベクトル   ディレクレ分布のハイパーパラメータ
     _maxiter = 100
+    _a = None # K次元ベクトル   ガンマ分布のパラメータaのベクトル
+    _b = None # K次元ベクトル   ガンマ分布のパラメータbのベクトル
 
     def __init__(self, lambda_vector, pi_vector, alpha_vector, num):
         self.lambda_vector = lambda_vector # 初期設定
@@ -26,11 +29,14 @@ class PoissonMixedModel:
         N, K = len(X.T), len(self.lambda_vector) # データ数とクラスタ数
         S = np.zeros((K, N)) # K*N行列，1ofK表現    クラスタの割り当ての行列
         eta = np.zeros((K, N)) # K*N行列    カテゴリ分布のパラメータ
-        a, b = np.zeros(K), np.zeros(K) # K次元ベクトル ガンマ分布のパラメータ
-        for _ in range(self._maxiter):
+        self._a, self._b = np.zeros(K), np.zeros(K) # K次元ベクトル ガンマ分布のパラメータ
+        for i in range(self._maxiter):
+            print(i+1, "回目のサンプリング")
             self._sample_s(X, S, eta, N, K)
-            self._sample_lambda(X, S, N, K, a, b, a_0, b_0)
+            self._sample_lambda(X, S, N, K, a_0, b_0)
             self._sample_pi(S, N, K)
+        print(self.lambda_vector)
+        print(self.pi_vector)
         return S
 
     def _sample_s(self, X, S, eta, N, K):
@@ -44,32 +50,32 @@ class PoissonMixedModel:
         for n in range(N):
             for k in range(K):
                 eta[k,n] = np.exp(X[0,n] * np.log(self.lambda_vector[k]) - self.lambda_vector[k] + np.log(self.pi_vector[k]))
+            sum_eta_n = sum(eta[:,n])
             for k in range(K):
-                eta[k,n] = eta[k,n] / sum(eta[k,:])          
-            # todo snをサンプル
-            custm = stats.rv_discrete(name='custm', values=(xk, eta[n]))
+                eta[k,n] = eta[k,n] / sum_eta_n          
+            # snをサンプル
+            custm = stats.rv_discrete(name='custm', values=(xk, eta[:,n]))
             rnd = custm.rvs(size=1)
-            S[n] = np.array([1 if (k == rnd[0]) else 0 for k in range(K)])
-        return S
+            S[:,n] = np.array([1 if (k == rnd[0]) else 0 for k in range(K)])
+        return
 
-    def _sample_lambda(self, X, S, N, K, a, b, a_0, b_0):
+    def _sample_lambda(self, X, S, N, K, a_0, b_0):
         """ lambda[n]をサンプルする
             Parameter
                 X       : 1*N行列   入力データ. 1次元だが行列として扱う. 
                 S       : K*N行列   予測した各クラスの割り当て
                 N       : データ数
-                K       : クラスタ数
-                a, b    : K次元ベクトル ガンマ分布のパラメータ """
-        sum1 = 0
-        sum2 = 0
+                K       : クラスタ数 """
         for k in range(K):
+            sum1 = 0
+            sum2 = 0
             for n in range(N):
                 sum1 += S[k,n] * X[0,n]
                 sum2 += S[k,n]
-            a[k] = sum1 + a_0
-            b[k] = sum2 + b_0
-            # todo lambda_kをサンプル
-            self.lambda_vector[k] = np.random.gamma(a[k], scale=1/b[k])
+            self._a[k] = sum1 + a_0
+            self._b[k] = sum2 + b_0
+            # lambda_kをサンプル
+            self.lambda_vector[k] = np.random.gamma(self._a[k], scale=1/self._b[k])
         return
 
     def _sample_pi(self, S, N, K):
@@ -81,9 +87,37 @@ class PoissonMixedModel:
         for k in range(K):
             for n in range(N):
                 self._alpha_vector[k] += S[k,n]
-        # todo pi_vectorをサンプル
+        # pi_vectorをサンプル
         self.pi_vector = np.random.dirichlet(self._alpha_vector)
         return
+
+    def predict(self, new_X):
+        """ 新規入力データの出現確率を推定
+            Parameter
+                new_X       : 1*M行列   入力データ. 1次元だが行列として扱う.  """
+        M, K = len(new_X.T), len(self._a) # 入力データ数とクラスタ数
+        r, p = np.zeros(K), np.zeros(K) # 負の二項分布のパラメータ
+        prob = np.zeros((K,M)) # 各クラスタからの生成確率
+        for k in range(K):
+            r[k] = self._a[k]
+            p[k] = 1 / (self._b[k] + 1)
+            print(r[k])
+            print(p[k])
+            for m in range(M):
+                equ1 = 0.0
+                equ2 = 0.0
+                print(k, m, "の計算")
+                for x in range(1, int(new_X[0,m]+1)):
+                    equ1 += math.log(x + r[k])
+                print("equ1", equ1)
+                for x in range(1, int(new_X[0,m]+1)):
+                    equ2 += math.log(x)
+                print("equ2", equ2)
+                equ3 = r[k] * math.log(1-p[k]) + new_X[0,m] * math.log(p[k])
+                print("equ3", equ3)
+                prob[k,m] = math.exp(equ1 - equ2 + equ3)
+                print("finish all")
+        return prob
 
     def get_lambda_vector(self):
         return self.lambda_vector
